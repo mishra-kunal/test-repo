99550e8983f8a65eeb1d24df73bf5287a3ea9f5c
Kunal Mishra <39880962+mishra-kunal@users.noreply.github.com>
2023-09-19 11:30:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
-print(2+2)
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
